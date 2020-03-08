aefe677fe03034cb93cea1bb276bc4f090b25209
a-drain <acsmith11@dons.usfca.edu>
2020-03-08 04:05:45 -0700

    Fixing cached files

--- test/TestHelpfulOperators.py
+++ test/TestHelpfulOperators.py
@@ -1,16 +1,15 @@
 import unittest
-import HelpfulOperators
 
-
+from HelpfulOperators import rewind, makeEqualities
 
 class TestHelpfulOperators(unittest.TestCase):
 
     def testMakeEqualities(self):
-        testBTC =  ['BTCUSDT_OHLCV_15m', 'stochrsi_BTCUSDT_15m']
+        testBTC = ['BTCUSDT_OHLCV_15m', 'stochrsi_BTCUSDT_15m']
         expected_query1 = "WHERE BTCUSDT_OHLCV_15m.timestamp = stochrsi_BTCUSDT_15m.timestampstochrsi ;"
         expected_query2 = " ALTER TABLE mytable DROP COLUMN timestampstochrsi ;"
 
-        result1, result2 = HelpfulOperators.makeEqualities(testBTC)
+        result1, result2 = makeEqualities(testBTC)
         self.assertEqual(result1, expected_query1 , 'Correct WHERE statement generated')
         self.assertEqual(result2, expected_query2, 'Correct ALTER TABLE statement generated')
 
