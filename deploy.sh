f46a739f89d0d354c2cb02c1a3b80205f2b4d849
a-drain <acsmith11@dons.usfca.edu>
2020-03-12 03:18:46 -0700

    Fixing ssh

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+tar -zcvf ccxt-trader.tar.gz src/
+
+scp -o StrictHostKeyChecking=no ccxt-trader.tar.gz ec2-user@ec2-34-219-56-114.us-west-2.compute.amazonaws.com:/home/ec2-user
+scp -o StrictHostKeyChecking=no requirements.txt ec2-user@ec2-34-219-56-114.us-west-2.compute.amazonaws.com:/home/ec2-user
+
+ssh -o StrictHostKeyChecking=no ec2-user@ec2-34-219-56-114.us-west-2.compute.amazonaws.com tar -xvzf ccxt-trader.tar.gz
