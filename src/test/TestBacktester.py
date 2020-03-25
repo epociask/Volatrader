088e8b3ba763f6e808f817581acb5a96d202f005
ethen <empociask@dons.usfca.edu>
2020-03-25 18:38:26 -0500

    c

--- src/test/TestBacktester.py
+++ src/test/TestBacktester.py
@@ -1,11 +1,11 @@
-import unittest
-from BackTest.backtester import backTest
-from Helpers.Enums import Pair, Candle, Time
-class TestBacktester(unittest.TestCase):
-
-    def test(self):
-        try:
-            backTest(Pair.ETHUSDT, Candle.FIVE_MINUTE, "SIMPLE_BUY_STRAT", 2, 4, 10000, Time.DAY)
-        except Exception as e:
-            self.assertFalse(e)
-
+# import unittest
+# from BackTest.backtester import backTest
+# from Helpers.Enums import Pair, Candle, Time
+# class TestBacktester(unittest.TestCase):
+#
+#     def test(self):
+#         try:
+#             backTest(Pair.ETHUSDT, Candle.FIVE_MINUTE, "SIMPLE_BUY_STRAT", 2, 4, 10000, Time.DAY)
+#         except Exception as e:
+#             self.assertFalse(e)
+#
