f3a685cc0f61cef3298cfbc5983da89b71c06f29
a-drain <acsmith11@dons.usfca.edu>
2020-04-04 04:46:48 -0700

    Deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,6 @@
-tar -zcvf ccxt-trader.tar.gz src/
+tar -zcvf taapi-js.tar.gz taapi-js/
 
-scp -o StrictHostKeyChecking=no ccxt-trader.tar.gz root@159.65.75.241:/root/
-scp -o StrictHostKeyChecking=no requirements.txt root@159.65.75.241:/root/
+scp -o StrictHostKeyChecking=no taapi-js.tar.gz root@159.65.75.241:/root/
 
-ssh -o StrictHostKeyChecking=no root@159.65.75.241 tar -xvzf ccxt-trader.tar.gz
-ssh -o StrictHostKeyChecking=no root@159.65.75.241 'touch src/database.ini ; echo "
-[postgresql]
-user=doadmin
-password=imt6kws2bm7ffay8
-host=coin-do-user-7113675-0.db.ondigitalocean.com
-port=25060
-database=defaultdb
-sslmode=require" > src/database.ini'
-
+ssh -o StrictHostKeyChecking=no root@159.65.75.241 tar -xvzf taapi-js.tar.gz
+ssh -o StrictHostKeyChecking=no root@159.65.75.241 'cd taapi-js && npm install && tmux && node index.js'
