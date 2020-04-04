97d57a35efd0cda67f3a522a9a5314a366b31025
ethen <empociask@dons.usfca.edu>
2020-04-04 06:19:42 -0500

    deleted tests

--- src/DB/QueryHelpers.py
+++ src/DB/QueryHelpers.py
@@ -8,6 +8,7 @@ clean2 = lambda val: val if val.find('2') == -1 else val.replace("2", "two")
 clean100 = lambda val: val if val.find('100') == -1 else val.replace("2", "hundred")
 getIndicatorName = lambda indicator: Indicator(indicator.value).name.lower()  # Gets indicator key from enum
 
+
 def getCandlesWithIndicatorsFromDBQuery(pair: Pair, candleSize: Candle, limit: (int, None)):
     """
     Gets OHLCV candle data from database
@@ -16,7 +17,9 @@ def getCandlesWithIndicatorsFromDBQuery(pair: Pair, candleSize: Candle, limit: (
     :param limit: either None or integer --> specifies amount of rows to return if not none
     :return: str
     """
-    return f"SELECT * FROM {pair.value}_OHLCV_{candleSize.value} ORDER BY TIMESTAMP DESC " + (";" if limit is None else f"LIMIT {limit};")
+    return f"SELECT * FROM {pair.value}_OHLCV_{candleSize.value} ORDER BY TIMESTAMP DESC " + (
+        ";" if limit is None else f"LIMIT {limit};")
+
 
 # def getInsertBackTestDataQuery(session:, candleSize: Candle, start: str, finish: str):
 #     """
@@ -38,5 +41,3 @@ def getCreateBackTestTableQuery() -> str:
            f"PAIR VARCHAR, CANDLESIZE VARCHAR, STRATEGY VARCHAR, POSTIVE_TRADES VARCHAR, NEGATIVE_TRADES VARCHAR, START_TIME VARCHAR, FINISH_TIME VARCHAR, " \
            f"STOP_LOSS_PERCENT VARCHAR, TAKE_PROFIT_PERCENT VARCHAR, PROFIT_LOSS VARCHAR " \
            f", CONSTRAINT pkey_tbl PRIMARY KEY ( pkey ));"
-
-
