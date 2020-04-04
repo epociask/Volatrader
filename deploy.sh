a74c8ddef5a55c9eeed258bce28db216b49f2a74
a-drain <acsmith11@dons.usfca.edu>
2020-04-04 05:55:33 -0700

    deploy fixed?

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
-tar -zcvf taapi-js.tar.gz taapi-js/
+cd src && tar -zcvf taapi-js.tar.gz taapi-js/
 
 scp -o StrictHostKeyChecking=no taapi-js.tar.gz root@159.65.75.241:/root/
 
 ssh -o StrictHostKeyChecking=no root@159.65.75.241 tar -xvzf taapi-js.tar.gz
-ssh -o StrictHostKeyChecking=no root@159.65.75.241 'cd taapi-js && npm install && tmux && node index.js'
+ssh -o StrictHostKeyChecking=no root@159.65.75.241 'cd taapi-js && npm install && forever stopall && NODE_TLS_REJECT_UNAUTHORIZED='0' forever start index.js'
+rm -rf taapi-js.tar.gz
+echo 'done'
