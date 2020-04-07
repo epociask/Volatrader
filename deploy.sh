0d0dc6c68c7491e5e6585638a270e3a85f91fd52
a-drain <acsmith11@dons.usfca.edu>
2020-04-06 21:55:35 -0700

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
-cd src && tar -zcvf taapi-js.tar.gz taapi-js/
+tar --exclude node_modules -zcvf ccxt-trader.tar.gz src/
 
-scp -o StrictHostKeyChecking=no taapi-js.tar.gz root@159.65.75.241:/root/
+scp -o StrictHostKeyChecking=no ccxt-trader.tar.gz root@159.65.75.241:/root/
 
-ssh -o StrictHostKeyChecking=no root@159.65.75.241 tar -xvzf taapi-js.tar.gz
-ssh -o StrictHostKeyChecking=no root@159.65.75.241 'cd taapi-js && npm install && forever stopall && npm run start'
-rm -rf taapi-js.tar.gz
+ssh -o StrictHostKeyChecking=no root@159.65.75.241 tar -xvzf ccxt-trader.tar.gz
+ssh -o StrictHostKeyChecking=no root@159.65.75.241 'nohup python3.7 src/priceNotificationsDriver.py >/dev/null 2>&1 &'
+ssh -o StrictHostKeyChecking=no root@159.65.75.241 'cd src/taapi-js && npm install && npm run start-prod'
+rm -rf ccxt-trader.tar.gz
 echo 'done'
