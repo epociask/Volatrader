e39383dbc3e7db14131353cb0737e7adccc9edab
ethen <empociask@dons.usfca.edu>
2020-04-08 04:16:54 -0500

    better

--- src/priceNotificationsDriver.py
+++ src/priceNotificationsDriver.py
@@ -24,22 +24,23 @@ def startQueue():
 
 
 if __name__ == '__main__':
+    try:
+        while True:
+            t = int(str(datetime.now())[14:16])
+            if not hasRun or t == 0:
+                movers = getTopPercentChange("1h", 20, 10000)
+                logToSlack(f"Hourly Top price movers:\n {movers}", channel=Channel.VOLATRADER)
 
-    while True:
-        t = int(str(datetime.now())[14:16])
-        if not hasRun or t == 0:
-            movers = getTopPercentChange("1h", 20, 10000)
-            logToSlack(f"Hourly Top price movers:\n {movers}", channel=Channel.VOLATRADER)
-
-            emptyQueue()
-            if not hasRun:
-                hasRun = True
-            l = getTopVolumeCoins(12)
-            for coin in l:
-                que.append(Thread(target=sendAbnormalVolumeNotification, args=(coin,),))
-            
-            startQueue()
+                emptyQueue()
+                if not hasRun:
+                    hasRun = True
+                l = getTopVolumeCoins(12)
+                for coin in l:
+                    que.append(Thread(target=sendAbnormalVolumeNotification, args=(coin,),))
+                
+                startQueue()
 
 
-
+    except Exception as e:
+        logToSlack(e)
 
